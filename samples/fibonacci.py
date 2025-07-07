2e12337de6764e55705a567ffedd7b80e953fea2
Maya Alexander <maya.alexander@duke.edu>
2025-07-07 14:23:13 -0400

    samples and secrets changed

--- samples/fibonacci.py
+++ samples/fibonacci.py
@@ -1,13 +1,7 @@
-# DOC_TITLE: Fibonacci Sequence Example
-# DOC_SUMMARY: Computes the first N Fibonacci numbers.
-# DOC_NOTES: This sample demonstrates a basic loop and list usage.
-
-### Define Fibonacci Function
 def fibonacci(n):
     seq = [0, 1]
     for i in range(2, n):
         seq.append(seq[-1] + seq[-2])
     return seq[:n]
 
-### Print First 10 Fibonacci Numbers
 print(fibonacci(10)) 
