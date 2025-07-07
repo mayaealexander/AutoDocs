b183efe721c20059d3d77474927dd5a6533078d2
Maya Alexander <maya.alexander@duke.edu>
2025-07-07 14:04:06 -0400

    edit hello world

--- samples/hello_world.py
+++ samples/hello_world.py
@@ -1,6 +1,1 @@
-# DOC_TITLE: Hello World Example
-# DOC_SUMMARY: A simple hello world script for testing AutoDocs.
-# DOC_NOTES: This is a test sample to verify documentation generation.
-
-### Print Hello World
 print("Hello, world!") 
