5e49e696f0d9a02bebe1d53af5413221f1ccf41f
mayaealexander <89485222+mayaealexander@users.noreply.github.com>
2025-07-20 02:50:39 +0000

    Add inline comments via AI assistant [skip comment]

--- samples/hello_world.py
+++ samples/hello_world.py
@@ -1,2 +1,8 @@
-print("Hello, world!")
-print("Goodbye!")
+# DOC_TITLE: Hello World and Goodbye Example
+# DOC_BLURB: Simple Python script printing two messages.
+# DOC_NOTE: This example demonstrates basic use of the print function.
+
+### Print greetings
+print("Hello, world!")  # Output a greeting message
+
+print("Goodbye!")       # Output a farewell message
