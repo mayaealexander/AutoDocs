923caf87a954ccd33235dbcb440c9c78bf1803d4
Maya Alexander <maya.alexander@duke.edu>
2025-07-20 22:23:25 -0400

    testing doc updates/overwrites

--- samples/hello_world.py
+++ samples/hello_world.py
@@ -1,5 +1,2 @@
-# DOC_TITLE: Print "Hello, world!" in Python
-# DOC_SUMMARY: The classic first program to display text output.
-# DOC_NOTE: This example demonstrates how to use the built-in print function in Python.
-
-print("Hello, world!")  # Output greeting to the console
+print("Hello, world!")
+print("test 123")
