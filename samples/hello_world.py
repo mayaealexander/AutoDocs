a6a19e7b07faf9a03be24a3bec74661c2b4fee75
Maya Alexander <maya.alexander@duke.edu>
2025-07-27 15:48:05 -0400

    issues resolved (some)

--- samples/hello_world.py
+++ samples/hello_world.py
@@ -1,8 +1,1 @@
-# DOC_TITLE: Printing Output in Python
-# DOC_SUMMARY: Simple examples of using print statements in Python.
-# DOC_NOTE: The print() function outputs text to the console.
-# DOC_LINKS: [Python print() documentation](https://docs.python.org/3/library/functions.html#print)
-
-### Print greetings and test message
-print("Hello, world!")  # Print a greeting message
-print("test 123")       # Print a test message with numbers
+print("Hello, world!") 
