d4cdf26dd89deb9c439fd3c292432de5fb2a9949
Maya Alexander <maya.alexander@duke.edu>
2025-08-17 16:54:53 -0400

    test change

--- samples/hello_world.py
+++ samples/hello_world.py
@@ -1,8 +1,1 @@
-# DOC_TITLE: Hello World Example in Python
-# DOC_SUMMARY: A minimal Python script that prints a greeting message.
-# DOC_NOTE: This is the classic "Hello, world!" example, often used as a first program in learning a new language.
-# DOC_LINKS: [Python print() function](https://docs.python.org/3/library/functions.html#print)
-
-### Print a greeting message
-# DOC_STEP_SUMMARY: This step outputs the string "Hello, world!" to the console using Python's built-in print function.
-print("Hello, world!")  # Output greeting to the console
+print("Hello, world!")  
